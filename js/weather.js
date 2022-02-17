5790207e3015ba2750d2e9a3b75e8b12f5b552c4
chaghantak <dufsodaus@gmail.com>
2022-02-17 18:00:48 +0900

    third commit

--- js/weather.js
+++ js/weather.js
@@ -2,10 +2,10 @@ const API_KEY= "f14ad9eacf49779cf4213aec9d85e2cb"
 
 //37.4795091 127.1151263
 function geoOk(position){
-    // const lat= position.coords.latitude
-    // const lon= position.coords.longitude
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f14ad9eacf49779cf4213aec9d85e2cb&units=metric`
-    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+     const lat= position.coords.latitude
+     const lon= position.coords.longitude 
+    console.log(lat,lon)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
    fetch(url)
    .then((res)=>res.json())
    .then((data)=>{
